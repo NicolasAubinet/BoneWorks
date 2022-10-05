39f248140be612e2aa67eb628c39d50fce1e2996
Nicolas Aubinet <nicolas@abrakam.com>
2022-10-05 13:29:17 +0200

    Add empty hooks skeleton to prepare to add action-specific haptic feedbacks

---
+++ ShockwaveBoneWorks/Hooks.cs
@@ -0,0 +1,100 @@
+﻿using StressLevelZero.Combat;
+using StressLevelZero.Interaction;
+using StressLevelZero.Props.Weapons;
+using StressLevelZero.VRMK;
+using UnityEngine;
+
+namespace ShockwaveBoneWorks
+{
+    public class Hooks
+    {
+        //Handles Player melee hits when holding an object/weapon
+        private static void Haptic_Hit(Haptor __instance, float amp)
+        {
+        }
+
+        private static void PlayerAttacked(PlayerDamageReceiver __instance, Attack attack)
+        {
+        }
+
+        private static void SENDHAPTIC(Haptor __instance, float delay, float duration, float frequency, float amplitude)
+        {
+        }
+
+        private static void TelekinesisActivateFunc(bool leftHand)
+        {
+        }
+
+        private static void ForcePullGripOnStartAttach(Hand hand)
+        {
+        }
+
+        private static void ForcePullGripOnForcePullComplete(Hand hand)
+        {
+        }
+
+        private static void ForcePullGripCancelPull(Hand hand)
+        {
+        }
+
+        private static void ForcePullGripOnFarHandHoverEnd(Hand hand)
+        {
+        }
+
+        private static void ArenaBalloonGunOnFire(ArenaBalloonGun __instance)
+        {
+        }
+
+        private static void BalloonGunOnFire(BalloonGun __instance)
+        {
+        }
+
+        private static void DevManipulatorGunBlast(DevManipulatorGun __instance)
+        {
+        }
+
+        private static void DevManipulatorGunOnBallGripUpdate(DevManipulatorGun __instance, Hand hand)
+        {
+        }
+
+        private static void FlyingGunOnTriggerGripUpdate(FlyingGun __instance, Hand hand)
+        {
+        }
+
+        private static void GrapplingHookFire(GrapplingHook __instance)
+        {
+        }
+
+        private static void GravityGunGrab(StressLevelZero.Interaction.GravityGun __instance)
+        {
+        }
+
+        private static void GravityGunPush(StressLevelZero.Interaction.GravityGun __instance)
+        {
+        }
+
+        private static void GravityGunPull(StressLevelZero.Interaction.GravityGun __instance)
+        {
+        }
+
+        private static void GrenadeLauncherOnFire(GrenadeLauncher __instance)
+        {
+        }
+
+        private static void SpawnGunOnFire(SpawnGun __instance)
+        {
+        }
+
+        private static void PhysGrounderCollisionEnter(PhysGrounder __instance, Collision c)
+        {
+        }
+
+        private static void HandWeaponSlotReceiverHoverBegin(HandWeaponSlotReciever __instance, Hand hand)
+        {
+        }
+
+        private static void HandWeaponSlotReceiverHoverEnd(HandWeaponSlotReciever __instance, Hand hand)
+        {
+        }
+    }
+}
--- ShockwaveBoneWorks/ShockwaveBoneWorks.cs
+++ ShockwaveBoneWorks/ShockwaveBoneWorks.cs
@@ -1,44 +1,123 @@
-﻿using MelonLoader;
+﻿using Il2CppSystem.Reflection;
+using MelonLoader;
 using UnityEngine;
 using ModThatIsNotMod.MonoBehaviours;
 using ModThatIsNotMod;
+using StressLevelZero.Interaction;
+using StressLevelZero.Props.Weapons;
 using StressLevelZero.VRMK;
+
 namespace ShockwaveBoneWorks
 {
  
     public static class BuildInfo
     {
-        public const string Name = "Shockwave"; // Name of the Mod.  (MUST BE SET)
-        public const string Author = "Akshay"; // Author of the Mod.  (Set as null if none)
-        public const string Company = "Shockwave VR" ; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
-        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        public const string Name = "Shockwave";
+        public const string Author = "Akshay & Alchemist";
+        public const string Company = "Shockwave VR";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
     }
 
     public class ShockwaveBoneWorks : MelonMod
     {
         ShockwaveManager suit;
-       Animator playerAnimator;
-      Transform playArea;
-   
+        Animator playerAnimator;
+        Transform playArea;
         Transform Physbody;
+
         public override void OnApplicationStart()
         {
          //   CustomMonoBehaviourHandler.RegisterMonoBehaviourInIl2Cpp<AvatarTransform_IK>();
             CustomMonoBehaviourHandler.RegisterMonoBehaviourInIl2Cpp<ShockwaveCollider>();
             suit = ShockwaveManager.Instance;
             suit.InitializeSuit();
-           
-          
+
+            RegisterHooks();
+
             MelonLogger.Msg("OnApplicationStart");
         }
+
+        void RegisterHooks()
+        {
+            Hooking.OnPostFireGun += GunHooks_OnGunFire;
+
+            const System.Reflection.BindingFlags originalBindingFlags =
+                (System.Reflection.BindingFlags)(BindingFlags.Instance | BindingFlags.Public);
+            const System.Reflection.BindingFlags hookBindingFlags =
+                (System.Reflection.BindingFlags)(BindingFlags.Static | BindingFlags.NonPublic);
+
+            Hooking.CreateHook(
+                typeof(PlayerDamageReceiver).GetMethod("ReceiveAttack", originalBindingFlags),
+                typeof(Hooks).GetMethod("PlayerAttacked", hookBindingFlags), true);
+            Hooking.CreateHook(
+                typeof(ForcePullGrip).GetMethod("OnStartAttach", originalBindingFlags),
+                typeof(Hooks).GetMethod("ForcePullGripOnStartAttach", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(ForcePullGrip).GetMethod("OnFarHandHoverEnd", originalBindingFlags),
+                typeof(Hooks).GetMethod("ForcePullGripOnFarHandHoverEnd", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(ForcePullGrip).GetMethod("OnForcePullComplete", originalBindingFlags),
+                typeof(Hooks).GetMethod("ForcePullGripOnForcePullComplete", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(ForcePullGrip).GetMethod("CancelPull", originalBindingFlags),
+                typeof(Hooks).GetMethod("ForcePullGripCancelPull", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(HandWeaponSlotReciever).GetMethod("OnHandHoverBegin", originalBindingFlags),
+                typeof(Hooks).GetMethod("HandWeaponSlotRecieverHoverBegin", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(HandWeaponSlotReciever).GetMethod("OnHandHoverEnd", originalBindingFlags),
+                typeof(Hooks).GetMethod("HandWeaponSlotRecieverHoverEnd", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(PhysGrounder).GetMethod("OnCollisionEnter", originalBindingFlags),
+                typeof(Hooks).GetMethod("PhysGrounderCollisionEnter", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(ArenaBalloonGun).GetMethod("OnFire", originalBindingFlags),
+                typeof(Hooks).GetMethod("ArenaBalloonGunOnFire", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(BalloonGun).GetMethod("OnFire", originalBindingFlags),
+                typeof(Hooks).GetMethod("BalloonGunOnFire", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(DevManipulatorGun).GetMethod("Blast", originalBindingFlags),
+                typeof(Hooks).GetMethod("DevManipulatorGunBlast", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(DevManipulatorGun).GetMethod("OnBallGripUpdate", originalBindingFlags),
+                typeof(Hooks).GetMethod("DevManipulatorGunOnBallGripUpdate", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(GrapplingHook).GetMethod("FireHook", originalBindingFlags),
+                typeof(Hooks).GetMethod("GrapplingHookFire", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(StressLevelZero.Interaction.GravityGun).GetMethod("Grab", originalBindingFlags),
+                typeof(Hooks).GetMethod("GravityGunGrab", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(StressLevelZero.Interaction.GravityGun).GetMethod("Push", originalBindingFlags),
+                typeof(Hooks).GetMethod("GravityGunPush", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(StressLevelZero.Interaction.GravityGun).GetMethod("Pull", originalBindingFlags),
+                typeof(Hooks).GetMethod("GravityGunPull", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(GrenadeLauncher).GetMethod("OnFire", originalBindingFlags),
+                typeof(Hooks).GetMethod("GrenadeLauncherOnFire", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(SpawnGun).GetMethod("OnFire", originalBindingFlags),
+                typeof(Hooks).GetMethod("SpawnGunOnFire", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(FlyingGun).GetMethod("OnTriggerGripUpdate", originalBindingFlags),
+                typeof(Hooks).GetMethod("FlyingGunOnTriggerGripUpdate", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(Haptor).GetMethod("Haptic_Hit", originalBindingFlags),
+                typeof(Hooks).GetMethod("Haptic_Hit", hookBindingFlags));
+            Hooking.CreateHook(
+                typeof(Haptor).GetMethod("SENDHAPTIC", originalBindingFlags),
+                typeof(Hooks).GetMethod("SENDHAPTIC", hookBindingFlags));
+
+        }
+
         void SuitAdded(GameObject suitObject)
         {
-            
             MelonLogger.Msg("OnCustomItemAdded: " + suitObject.name);
-          
-        
         }
+
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             MelonLogger.Msg("OnLevelWasLoaded: " + sceneName);
@@ -51,7 +130,7 @@ namespace ShockwaveBoneWorks
          
             Physbody = GameObject.FindObjectOfType<StressLevelZero.Rig.PhysicsRig>().transform;
             playerAnimator = GameObject.FindObjectOfType<StressLevelZero.Player.CharacterAnimationManager>().gameObject.GetComponent<Animator>(); ;
-          playArea= GameObject.FindObjectOfType<StressLevelZero.Rig.SteamControllerRig>().transform.GetChild(0);
+            playArea = GameObject.FindObjectOfType<StressLevelZero.Rig.SteamControllerRig>().transform.GetChild(0);
        //     CapsuleCollider[] torsoColliders = Physbody.GetChild(3).GetComponentsInChildren<CapsuleCollider>();
        //     BoxCollider[] chestFront = Physbody.GetChild(3).GetComponentsInChildren<BoxCollider>();
             CapsuleCollider legCollider = Physbody.GetChild(4).GetComponentInChildren<CapsuleCollider>();
@@ -59,11 +138,8 @@ namespace ShockwaveBoneWorks
             shockwaveCollider.animator = playerAnimator;
             shockwaveCollider.bodyForward = playArea.forward;
             shockwaveCollider.region = ColliderRegion.PELVIS;
-            
-           
-    
-                
-               ShockwaveCollider leftThighCollider = legCollider.transform.parent.gameObject.AddComponent<ShockwaveCollider>();
+
+            ShockwaveCollider leftThighCollider = legCollider.transform.parent.gameObject.AddComponent<ShockwaveCollider>();
             ShockwaveCollider leftCalfCollider = legCollider.transform.parent.gameObject.AddComponent<ShockwaveCollider>();
             ShockwaveCollider rightThighCollider = legCollider.transform.parent.gameObject.AddComponent<ShockwaveCollider>();
             ShockwaveCollider rightCalfCollider = legCollider.transform.parent.gameObject.AddComponent<ShockwaveCollider>();
@@ -79,20 +155,15 @@ namespace ShockwaveBoneWorks
             leftCalfCollider.bodyForward = playArea.forward;
             rightThighCollider.bodyForward = playArea.forward;
             rightCalfCollider.bodyForward = playArea.forward;
-
-            
-
         }
        
-
         public override void OnApplicationQuit()
         {
             suit.DisconnectSuit();
         }
         
-        
+        private void GunHooks_OnGunFire(Gun gun)
+        {
+        }
     }
-
-
-
 }
